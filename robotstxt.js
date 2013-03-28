946d0a830d30be35a0449ae39acdbe7eb4eb1ea3
Erlend Røsjø <erlend.rosjo@knowit.no>
2013-03-28 19:31:17 +0100

    robotstxt.js

---
+++ robotstxt.js
@@ -0,0 +1,42 @@
+/*
+HTTP status 200 test for robots.txt
+*/
+
+//Url to test
+var url = "http://brage.no/robots.txt";
+
+//Init casper
+var casper = require('casper').create({
+	verbose: false,
+	logLevel: "info",
+	onError: function(self, m) {   // Any "error" level message will be written
+		console.log('FATAL:' + m); // on the console output and PhantomJS will
+		self.exit();               // terminate
+	},
+	pageSettings: {
+		loadImages:  false,        // The WebPage instance used by Casper will
+		loadPlugins: false         // use these settings
+	}
+});
+
+//Start casper using specified url
+casper.start(url);
+
+//HTTP 200 status test
+var httpStatus = function() {
+	this.test.assertHttpStatus(200, 'url returns 200 OK');
+};
+
+//Add navigation step
+casper.then(httpStatus);
+
+//Run all steps
+casper.run(function() {
+	//All steps complete
+
+	//Set test done
+	this.test.done();
+
+	//Render results to console
+	this.test.renderResults(true, 0);
+});
