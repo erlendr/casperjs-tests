d8429063cdd01363853feb6a6b8973c24bb7dc38
Erlend Røsjø <erlend.rosjo@knowit.no>
2013-03-28 16:43:27 +0100

    Simplified sitemap.js

--- sitemap.js
+++ sitemap.js
@@ -1,6 +1,6 @@
 /*
 Reads sitemap from specified url, fetches urls, opens them
-Exits with code 1 if 404 or 500
+Checks if title tag exists
 */
 var url = 'http://www.folkas.com/sitemap';
 
@@ -15,46 +15,38 @@ var casper = require('casper').create({
 		loadImages:  false,        // The WebPage instance used by Casper will
 		loadPlugins: false         // use these settings
 	},
-	httpStatusHandlers: {
-		404: function(self, resource) {
-			this.echo("Resource at " + resource.url + " not found (404)", "COMMENT");
-			this.exit(1);
-		},
-		500: function(self, resource) {
-			this.echo("Resource at " + resource.url + " error (500)", "COMMENT");
-			this.exit(1);
-		}
-	},
 	viewportSize: {width: 1024, height: 768}
 });
 
-//Step to fetch sitemap xml
-casper.start().then(function() {
-	this.open(url, {
-		method: 'get'
-	});	
-});
+var urls = [];
 
-//Run above step
-casper.run(function() {
+//Start casper by fetching sitemap xml
+casper.start(url, function() {
 	//Parse sitemap xml
 	var parser = new DOMParser();
 	var doc = parser.parseFromString(this.getPageContent(), "application/xml");
 	
 	//Find all loc nodes, and store url inside in array
 	var locNodes = doc.querySelectorAll("loc");
-	var urls = [];
+
 	for(var i = 0; i < locNodes.length; i++) {
 		urls.push(locNodes[i].childNodes[0].data);
 	}
 
 	//Start new batch of tests using urls
 	casper.start().each(urls, function(self, url) {
-		//Open url, output title
 		self.thenOpen(url, function() {
-			this.echo(this.getTitle());
+		    this.test.assertExists('title', 'title tag exists');
 		});
 	});
 
-	casper.run();
+	casper.run(function() {
+		this.test.done();
+		this.test.renderResults(true);
+	});
 });
+
+
+
+//Run all steps
+casper.run();
